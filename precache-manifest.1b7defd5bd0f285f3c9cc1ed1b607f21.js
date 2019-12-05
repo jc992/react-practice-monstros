67ebc1afe3b316d8b39c4e93668ef00d081a1c1a
JoaoT <joaot@agap2.pt>
2019-12-05 12:49:27 -0100

    Updates

---
+++ precache-manifest.1b7defd5bd0f285f3c9cc1ed1b607f21.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f42b5fcb349e316cbfc28ab979565387",
+    "url": "/react-practice-monstros/index.html"
+  },
+  {
+    "revision": "c070bba29745c358b88f",
+    "url": "/react-practice-monstros/static/css/main.3f5c51a0.chunk.css"
+  },
+  {
+    "revision": "a534be9ee944c0014d67",
+    "url": "/react-practice-monstros/static/js/2.d3445554.chunk.js"
+  },
+  {
+    "revision": "c070bba29745c358b88f",
+    "url": "/react-practice-monstros/static/js/main.3d470e90.chunk.js"
+  },
+  {
+    "revision": "4b079bd43ffcd388764c",
+    "url": "/react-practice-monstros/static/js/runtime-main.c564b3df.js"
+  }
+]);
